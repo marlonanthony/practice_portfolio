74e9b156120404ca3454727ef50c0903cb44a48d
borgatajunky <mad1083@gmail.com>
2019-04-02 18:25:19 -0400

    refactored js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,9 @@
-// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn')
 const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
 
-// Set initial State of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu)
@@ -17,8 +15,6 @@ function toggleMenu() {
     menuNav.classList.add('show')
     menuBranding.classList.add('show')
     navItems.forEach(item => item.classList.add('show'))
-
-    // Set Menu State
     showMenu = true
   } else {
     menuBtn.classList.remove('close')
@@ -26,8 +22,6 @@ function toggleMenu() {
     menuNav.classList.remove('show')
     menuBranding.classList.remove('show')
     navItems.forEach(item => item.classList.remove('show'))
-
-    // Set Menu State
     showMenu = false
   }
 }
