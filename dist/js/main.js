3ce516194cd4516b948b6c194531ca1a9511e4fd
borgatajunky <mad1083@gmail.com>
2019-04-04 19:51:11 -0400

    added javascript to remove show class when user clicks away from menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,8 +7,18 @@ const navItems = document.querySelectorAll('.nav-item')
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu)
-
-function toggleMenu() {
+document.addEventListener('click', (e) => {
+  if(!e.target.closest('.menu-btn')) {
+    menuBtn.classList.remove('close')
+    menu.classList.remove('show')
+    menuNav.classList.remove('show')
+    menuBranding.classList.remove('show')
+    navItems.forEach(item => item.classList.remove('show'))
+    showMenu = false
+  }
+})
+function toggleMenu(e) {
+  
   if(!showMenu) {
     menuBtn.classList.add('close')
     menu.classList.add('show')
